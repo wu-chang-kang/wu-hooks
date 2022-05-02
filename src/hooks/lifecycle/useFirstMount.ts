d968aa2f57c2116335af367952bb51ff300cd6c7
wu-chang-kang <1147552381@qq.com>
2022-05-02 21:55:38 +0800

    add

---
+++ src/hooks/lifecycle/useFirstMount.ts
@@ -0,0 +1,12 @@
+import { useRef } from 'react';
+
+const useFirstMountState = (): boolean => {
+  const isFirst = useRef<boolean>(true);
+  if (isFirst.current) {
+    isFirst.current = false;
+    return true;
+  }
+  return isFirst.current;
+};
+
+export default useFirstMountState;
