69b994c975c31a2e40a8d23fe4b6855b7f41c6fc
wu-chang-kang <1147552381@qq.com>
2022-05-03 08:04:16 +0800

    fix

--- src/hooks/state/createMemo.ts
+++ src/hooks/state/createMemo.ts
@@ -3,5 +3,5 @@ import { useMemo } from 'react';
 const createMemo = <T extends GlobalFunction>(fn: T) => {
   return (...args: Parameters<T>) => useMemo<ReturnType<T>>(() => fn(...args), args);
 };
-
+ 
 export default createMemo;
